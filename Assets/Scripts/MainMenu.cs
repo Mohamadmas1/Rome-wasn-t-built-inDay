9cf7fc0c8106dd845cbc98a93392f145771a7ad3
shira88 <121285296+shira88@users.noreply.github.com>
2025-01-28 00:04:30 +0200

    sprites and stuff

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,9 +7,20 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private SceneField gameScene;
+    [SerializeField] private GameObject instructionsPanel;
 
     public void StartGame()
     {
         SceneManager.LoadScene(gameScene.Name);
+    }
+    
+    public void ShowInstructions()
+    {
+        instructionsPanel.SetActive(true);
+    }
+    
+    public void HideInstructions()
+    {
+        instructionsPanel.SetActive(false);
     }
 }
